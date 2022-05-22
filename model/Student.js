70fbb9b6733a3ca50a8630eee42e6e0a31117e74
TarunBabu <vegitarun1234@gmail.com>
2022-05-22 23:43:31 +0530

    updated graduation

--- model/Student.js
+++ model/Student.js
@@ -78,17 +78,14 @@ const StudentSchema = mongoose.Schema({
             cgpa: "",
             loc: "",
             allcgpa: {
-                type: Object,
-                default: {
-                    0: 0,
-                    1: 0,
-                    2: 0,
-                    3: 0,
-                    4: 0,
-                    5: 0,
-                    6: 0,
-                    7: 0,
-                }
+                0: 0,
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0,
+                6: 0,
+                7: 0,
             }
         }
     },
