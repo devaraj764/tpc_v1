9da20cbe0f3208e99d9ef065f2108d80a6434258
Devaraj764 <mdevaraju764@.com>
2022-06-21 16:36:46 +0530

    Changes

--- send-mail.js
+++ send-mail.js
@@ -1,26 +1,32 @@
 const nodemailer = require('nodemailer');
 
+
+const user = 'cseplacements@rguktsklm.ac.in';
+const pass = 'hnkpehvamzwvulvy';
+
 let mailTransporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'mdevaraju764@gmail.com',
-        pass: 'ifueftmfazhobqoo'
+        // user: 'mdevaraju764@gmail.com',
+        // pass: 'ifueftmfazhobqoo'
+        user: user,
+        pass: pass
     }
 });
 
-const sendMail = async(email, url) => {
+const sendMail = async (email, url) => {
 
     let details = {
-        from: "mdevaraju764@gmail.com",
+        from: user,
         to: email,
         subject: "Password Reset",
         html: `<h4>Reset your password</h4><p>Valid only for 10min</p><a href="${url}">Click here</a>`
     }
 
-    try{
+    try {
         await mailTransporter.sendMail(details);
         return true;
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return false;
     }
